233a23e56503323746df42959f242ffe096a47b3
François Caddet <francois.caddet@icloud.com>
2022-05-21 18:30:17 +0200

    update experiment

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,27 @@ async fn main() {
     for w in root.get_children().await.unwrap() {
         let app: Accessible = w.iface().await.unwrap();
         for a in app.get_children().await.unwrap() {
-            let gnomew: Accessible = a.iface().await.unwrap();
-            println!("{:#?}", gnomew.name().await);
-            println!("{:#?}", gnomew.get_interfaces().await);
-            println!("{:#?}", gnomew.get_relation_set().await);
-            println!("{:#?}", gnomew.get_role().await);
-            println!("{:#?}", gnomew.get_state().await);
+            //            let win: Accessible = a.iface().await.unwrap();
+            //            for w in win.get_children().await.unwrap() {
+            let access: Accessible = a.iface().await.unwrap();
+            let comp: Component = a.iface().await.unwrap();
+            comp.set_position(700, 700, at_spi::geometry::Coord::Window)
+                .await
+                .unwrap();
+            comp.set_size(700, 700).await.unwrap();
+
+            println!("{:#?}", access.name().await);
+            println!("{:#?}", access.get_interfaces().await);
+            println!("{:#?}", access.get_relation_set().await);
+            println!("{:#?}", access.get_role().await);
+            println!("{:#?}", access.get_state().await);
+
+            println!(
+                "{:?}",
+                comp.get_extents(at_spi::geometry::Coord::Screen).await
+            );
+            println!("{:?}", comp.get_mdizorder().await);
+            //            }
         }
     }
 }
